8bb7fc64538834d3a070033cc9098a55c2a64a92
Jacky56 <jackybanh1997@gmail.com>
2019-08-17 14:38:01 +0100

    include source folder

--- e.py
+++ e.py
@@ -1,20 +1,25 @@
 import numpy as np
 
-a = np.array([[2,1],[1, 2],[1,3]])
+from Linear import LinearRegression
+
+a = np.array([[2,5],[1, 2],[1,3]])
 print(np.cov(a[0],a[1]))
 
 
 x = a[0] - np.mean(a[0])
 y = a[1] - np.mean(a[1])
 
-def cov(x,y):
+
+def my_cov(x,y):
     covariance = np.empty([len(x),len(x)])
     for x_val in x:
         row = np.empty(len(x))
         for y_val in y:
             np.append(row, x_val * y_val / len(x))
-            np.append(covariance,row)
+        np.append(covariance,row)
     return covariance
 
-print(cov(x,y))
+
+print(my_cov(x,y))
+
 
---
+++ iris.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+from Linear import LinearRegression as linear
+
+
+dataset = pd.read_csv("source/iris_numeric_lables.data")
+df_X = dataset.iloc[:,0:4]
+df_y = dataset.iloc[:,4]
+model = linear.LinearRegression()
+
+model.fit(df_X,df_y)
+
+weight, weight_c = model.getWeights()
+print (weight , weight_c)
+
+print(model.predict(np.array([5.6,2.9,3.6,1.3])))
