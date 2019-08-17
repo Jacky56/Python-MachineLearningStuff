1b99925d4d3c6b326850eb33d484cc64dc228060
Jacky56 <jackybanh1997@gmail.com>
2019-08-17 18:22:19 +0100

    added sigmoid and soft max

--- iris.py
+++ iris.py
@@ -2,6 +2,10 @@ import pandas as pd
 import numpy as np
 from SupervisedModels import LinearRegression as linear
 
+#checks accuracy
+def validate(y_pred, y_real):
+    return 1 - np.divide(np.sum(np.abs(y_pred - y_real)), y_real.shape[0])
+
 dataset = pd.read_csv("source/iris_numeric_lables.data")
 df_X = dataset.iloc[:,0:4]
 df_y = dataset.iloc[:,4]
@@ -10,6 +14,7 @@ model = linear.LinearRegression()
 model.fit(df_X,df_y)
 
 weight, weight_c = model.getWeights()
-print (weight , weight_c)
 
-print(model.predict(np.array([5.6,2.9,3.6,1.3])))
+y_pred = model.predict(df_X)
+
+print(validate(y_pred, df_y))
