fixed cost function for logistic (softmax)

SupervisedModels/LogisticRegression.py
@@ -13,38 +13,34 @@ class LogisticRegression(LinearRegression):
             y[np.array(np.where(label == k)), k] = 1
 
         # w(K,N)
-        self.weight = np.ones((np.unique(label).shape[0],records.shape[1]))
+        self.weight = np.ones((np.unique(label).shape[0], records.shape[1]))
         # w_c(K,1)
         self.weight_c = np.ones(np.unique(label).shape[0])
         # self.weight = np.ones(X.shape[1])
         # self.weight_c = 1
 
         for i in range(0,10000):
-
             # calculates cost sum[ h(N,1) - (N,1) ]=1
             # w_c(K,1) = w_c(K,1) - sum x [ h(M,K) - y(M,K) ]=(1,K).T
             weight_c_new = self.weight_c - a*np.sum((self.h(X) - y)/X.shape[0], axis=0).T
             # w(N,1) = w(N,1) - divide[ (M,K).T.(M,N), M ]=(K,N)
-            weight_new = self.weight - a*np.divide(self.cost(X, y), X.shape[0])
+            weight_new = self.weight - a*np.divide(X.T.dot(self.h(X) - y).T + lamb*self.weight, X.shape[0])
             # update weights
             self.weight = weight_new
             self.weight_c = weight_c_new
             if i % 1000 == 0:
-                print(self.loss(X,y))
-
-    # (M,N)T.(M,K)=(N,K).T
-    def cost(self,X, y):
-        return X.T.dot(self.h(X) - y).T
+                print(self.cost(X,y))
 
     #1
-    def loss(self,X,y):
-        return np.sum(-np.log(self.h(X)).dot(y.T) - np.log(1 - self.h(X)).dot((1 - y).T))/X.shape[0]
+    def cost(self,X,y):
+        # sum [ h(M,K)*y(M,k) - h(M,K)*y(M,k) ]= 1
+        return np.sum(-np.log(self.h(X))*(y) - np.log(1 - self.h(X))*(1 - y))
 
     # h(M,K)
     def softmax(self, wX):
         # 0 X(M,K)/{sum Y [ X(M,K) ]=(M,1)} =(M,K)
         # sum_prob = np.array([np.sum(np.exp(wX).T, axis=0)]).T
-        sum_prob = np.array([np.sum(np.exp(wX), axis=1)]).T
+        sum_prob = np.array([np.sum(np.exp(wX).T, axis=0)]).T
         return np.array(np.divide(np.array(np.exp(wX)), sum_prob))
 
     # M,K
@@ -55,7 +51,7 @@ class LogisticRegression(LinearRegression):
     # (M,1)
     def predict(self, X):
         # (M,1)
-        return np.argmax(self.h(X),axis=1)
+        return np.argmax(self.h(X).T, axis=0)
 
     # K = number of classes
     # X(M,N).(K,N).T=(M,K)

iris.py
@@ -26,7 +26,7 @@ def logistic(x,y):
     weight, weight_c = model.getWeights()
     print(weight, weight_c)
     print(validate(y_pred, y))
-    print(np.unique(y_pred))
+    print(model.predict(np.array([5.1,3.4,1.5,0.23])))
 
 
 # PCa stuff