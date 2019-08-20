bugfix softmax

SupervisedModels/LogisticRegression.py
@@ -8,23 +8,30 @@ class LogisticRegression(LinearRegression):
     def fit(self, records, label,a=0.0001,lamb=1,epsilon=0.00001):
         X = np.array(records)
         y = np.array(label)
+        self.k = np.array(np.unique(y))
+        # w(K,N)
         self.weight = np.ones((np.unique(y).shape[0],X.shape[1]))
+        # w_c(K,1)
         self.weight_c = np.ones(np.unique(y).shape[0])
         # self.weight = np.ones(X.shape[1])
         # self.weight_c = 1
 
         for i in range(0,1000):
-            for w in np.arange(self.weight_c.shape[0]):
             # calculates cost sum[ h(N,1) - (N,1) ]=1
-                #  c(1) = c(1) - mean[ h(M,1) - y(M,1) ]=1
-                weight_c_new = self.weight_c[w] - a*np.mean((self.h(X) - y))
-                # w(N,1) = w(N,1) - divide[ (M,N)T.(M,1), M ]=(N,1)
-                weight_new = self.weight[w] - a*np.mean(self.cost(X,y) + self.regularization(self.weight[w],lamb),axis=0)
-                # update weights
-                self.weight[w] = weight_new
-                self.weight_c[w] = weight_c_new
+            #  c(1) = c(1) - mean[ h(M,1) - y(M,1) ]=1
+            y_k = self.getWeight(y)
+            weight_c_new = self.weight_c[y_k] - a*np.mean((self.h(X) - y))
+            # w(N,1) = w(N,1) - divide[ (M,N)T.(M,1), M ]=(N,1)
+            weight_new = self.weight[y_k] - a*np.mean(self.cost(X,y) + self.regularization(self.weight[y_k],lamb),axis=0)
+            # update weights
+            self.weight[y_k] = weight_new
+            self.weight_c[y_k] = weight_c_new
 
-    # (M,N)T.(M,1), M ]=(N,1)
+    #y(M,1) = (?,1)
+    def getWeight(self, y):
+        return np.array(np.argwhere(y == self.k))
+
+    # (M,N)T.(M,1)=(N,1)
     def cost(self,X, y):
         return X.T.dot(self.h(X) - y)
 
@@ -46,6 +53,6 @@ class LogisticRegression(LinearRegression):
         # return X.dot(self.weight[weight_key].T) + self.weight_c[weight_key]
 
     # K = number of classes
-    # X(M,N).(N.K)=(M,K)
+    # X(M,N).(K,N).T=(M,K)
     def w(self,X):
         return X.dot(self.weight.T) + self.weight_c